a61c375a30ec62b4efa08d34d7f90c7ced1f9777
nnasu <nikolas.makikangas@gmail.com>
2023-09-01 19:54:14 +0300

    Kotitehtava aluilleen.

--- Tehtavat/Kotitehtava1.cpp
+++ Tehtavat/Kotitehtava1.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+
 using namespace std;
 
-int main ()
+int main()
 {
-    cout << "koira" << endl;
+
+    cout << "RANDMAX = " << RAND_MAX <<endl;
+    cout << "Time = "<< time(0) <<endl;
+    srand(time(0)); // alustetaan satunnaislukugeneraattori
+
+    int satunnainenLuku = rand()%20;
+    int kayttajanNumero = -1;
+
+    while(kayttajanNumero != satunnainenLuku)
+    {
+       cout<<"Arvaa luku 0-19 valilta"<< endl;
+       cin>>kayttajanNumero;
+
+       cout<<"Annoit numeron = "<< kayttajanNumero<<endl;
+    }
+
+
+    return 0;
 }
